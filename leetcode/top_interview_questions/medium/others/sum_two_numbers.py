a64cf1c6d2f5812f9beac9f69d0f367b18b2d06b
Daniel Gerlach <daniel.gerlach1981@googlemail.com>
2020-04-12 09:01:50 +0200

    leetcode, add two integers with bit manipulation, my working solution

--- leetcode/top_interview_questions/medium/others/sum_two_numbers.py
+++ leetcode/top_interview_questions/medium/others/sum_two_numbers.py
@@ -17,16 +17,35 @@ from time import sleep
 
 
 class Solution:
-    def getSum(self, a: int, b: int) -> int:
+    def getSumPositive(self, a: int, b: int) -> int:
+        """Does not work with negative integers. Because integer overflow is
+in python not like in C or Java."""
         while b != 0:
             # sleep(0.3)
-            print("a: {} b: {}".format(bin(a), bin(b)))
             curry = a & b
             a = a ^ b
             b = curry << 1
-            # b &= (1 << 8) - 1
-            # a &= (1 << 8) - 1
         return a
+
+    def getSum(self, a: int, b: int) -> int:
+        def add(x, y, curry1):
+            if x & y:
+                return 1 if curry1 else 0, 1
+            if x | y:
+                return 0 if curry1 else 1, 1 if curry1 else 0
+            return curry1, 0
+
+        aBinaryString = "".join(reversed(toBinaryString(a)))
+        bBinaryString = "".join(reversed(toBinaryString(b)))
+        result = []
+        curry = 0
+        for i in range(len(aBinaryString)):
+            a1 = int(aBinaryString[i])
+            b1 = int(bBinaryString[i])
+            r, curry = add(a1, b1, curry)
+            result.append(r)
+
+        return toDecimal("".join([str(n) for n in reversed(result)]))
 
 
 Case = namedtuple("Case", ["a", "b", "expected"])
@@ -40,12 +59,64 @@ def testSum():
         Case(0, 1, 1),
         Case(0, 0, 0),
         Case(63, 64, 127),
-        Case(254, 1, 255),
-        # Case(-1, -1, -2),
-        # Case(-1, 1, 0),
-        # Case(-12, -8, -20),
+        Case(-1, -1, -2),
+        Case(-1, 1, 0),
+        Case(-12, -8, -20),
+        Case(-128, 0, -128),
+        Case(-128, -1, 127),  # overflow with 8-bits
+        Case(127, 1, -128),  # overflow with 8-bits
     ]
 
     for c in cases:
         actual = sol.getSum(c.a, c.b)
+        assert actual == c.expected, "Case: {}".format(c)
+
+
+def toBinaryString(x: int, bits=8) -> str:
+    """Return bits-length binary string representation of the given
+integer. If x is negative the two-complement is created.
+    """
+    return "".join(list(reversed([str((x >> i) & 1) for i in range(bits)])))
+
+
+def toDecimal(s: str) -> int:
+    if s[0] == "1":
+        # manually convert two complement string in order to get absolute value
+        s = "".join(["0" if c == "1" else "1" for c in s])
+        return -(int(s[1:], 2) + 1)
+    return int(s[1:], 2)
+
+
+SimpleCase = namedtuple("SimpleCase", ["x", "expected"])
+
+
+def testBinaryString():
+    cases = [
+        SimpleCase(6, "00000110"),
+        SimpleCase(14, "00001110"),
+        SimpleCase(127, "01111111"),
+        SimpleCase(1, "00000001"),
+        SimpleCase(-1, "11111111"),
+        SimpleCase(-2, "11111110"),
+        SimpleCase(-3, "11111101"),
+        SimpleCase(-4, "11111100"),
+    ]
+    for c in cases:
+        actual = toBinaryString(c.x, 8)
+        assert actual == c.expected, "Case: {}".format(c)
+
+
+def testToDecimal():
+    cases = [
+        SimpleCase("00000110", 6),
+        SimpleCase("00001110", 14),
+        SimpleCase("01111111", 127),
+        SimpleCase("00000001", 1),
+        SimpleCase("11111111", -1),
+        SimpleCase("11111110", -2),
+        SimpleCase("11111101", -3),
+        SimpleCase("11111100", -4),
+    ]
+    for c in cases:
+        actual = toDecimal(c.x)
         assert actual == c.expected, "Case: {}".format(c)
