86b9ad3ef8fa592567fef04f39b62b56ace5d43e
Daniel Gerlach <daniel.gerlach1981@googlemail.com>
2020-07-14 21:57:25 +0200

    daily coding challenge, p548 calculate angle between hour and minute hands on a analog clock

--- daily_coding_challenges/p548.py
+++ daily_coding_challenges/p548.py
@@ -9,9 +9,32 @@ Bonus: When, during the course of a day, will the angle be zero?
 
 """
 
+from collections import namedtuple
+
+DEGREE_HOUR = 30
+DEGREE_MINUTE = 6
+
 
 def clockAngle(time):
-    # split time to 2 ints
+    # split time into 2 ints
+    hour, minute = [int(val) for val in time.split(":")]
 
     # calculate angle
-    pass
+    hourFraction = DEGREE_HOUR * minute // 60  # hour hands move forward during hour
+    return abs(((hour * DEGREE_HOUR) + hourFraction) - (minute * DEGREE_MINUTE))
+
+
+Case = namedtuple("Case", ["time", "expected"])
+
+
+def test():
+    cases = [
+        Case("0:00", 0),
+        Case("3:30", 75),
+        Case("8:15", 157),
+        Case("8:45", 8),
+        Case("4:45", 128),
+    ]
+    for c in cases:
+        actual = clockAngle(c.time)
+        assert actual == c.expected, "Case: {}".format(c.time)
