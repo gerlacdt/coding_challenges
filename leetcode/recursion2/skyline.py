c3c53439e0fd1f20ea73c7e61aa5a1b4ae2d5eed
Daniel Gerlach <daniel.gerlach1981@googlemail.com>
2020-07-15 21:06:54 +0200

    leetcode, recursion2, skyline problem

---
+++ leetcode/recursion2/skyline.py
@@ -0,0 +1,94 @@
+"""A city's skyline is the outer contour of the silhouette formed by
+all the buildings in that city when viewed from a distance. Now
+suppose you are given the locations and height of all the buildings as
+shown on a cityscape photo (Figure A), write a program to output the
+skyline formed by these buildings collectively (Figure B).
+
+Buildings Skyline Contour
+
+The geometric information of each building is represented by a triplet
+of integers [Li, Ri, Hi], where Li and Ri are the x coordinates of the
+left and right edge of the ith building, respectively, and Hi is its
+height. It is guaranteed that 0 <= Li, Ri <= INT_MAX, 0 < Hi <= INT_MAX,
+and Ri - Li > 0. You may assume all buildings are perfect rectangles
+grounded on an absolutely flat surface at height 0.
+
+For instance, the dimensions of all buildings in Figure A are recorded
+as: [ [2 9 10], [3 7 15], [5 12 12], [15 20 10], [19 24 8] ] .
+
+The output is a list of "key points" (red dots in Figure B) in the
+format of [ [x1,y1], [x2, y2], [x3, y3], ... ] that uniquely defines a
+skyline. A key point is the left endpoint of a horizontal line
+segment. Note that the last key point, where the rightmost building
+ends, is merely used to mark the termination of the skyline, and
+always has zero height. Also, the ground in between any two adjacent
+buildings should be considered part of the skyline contour.
+
+For instance, the skyline in Figure B should be represented as:[ [2
+10], [3 15], [7 12], [12 0], [15 10], [20 8], [24, 0] ].
+
+Notes:
+
+The number of buildings in any input list is guaranteed to be in the range [0, 10000].
+The input list is already sorted in ascending order by the left x position Li.
+The output list must be sorted by the x position.
+There must be no consecutive horizontal lines of equal height in the output skyline. For instance, [...[2 3], [4 5], [7 5], [11 5], [12 7]...] is not acceptable; the three
+lines of height 5 should be merged into one in the final output as such: [...[2 3], [4 5], [12 7], ...]
+
+
+https://leetcode.com/explore/learn/card/recursion-ii/507/beyond-recursion/3006
+
+"""
+
+from collections import namedtuple
+from typing import List
+from heapq import heappush, heapify
+
+
+Event = namedtuple("Event", ["index", "height", "kind"])
+
+START = "START_EVENT"
+END = "END_EVENT"
+
+
+class Solution:
+    def getSkyline(self, buildings: List[List[int]]) -> List[List[int]]:
+        result: List[List[int]] = []
+        events = []
+        for start, end, height in buildings:
+            events.append(Event(start, height, START))
+            events.append(Event(end, height, END))
+        events.sort()
+
+        heap: List = []
+        currentHeight = 0
+        for i, e in enumerate(events):
+            index, height, kind = e
+            if kind == START:
+                heappush(heap, -height)
+            elif kind == END:
+                heap.remove(-height)
+                heapify(heap)
+            else:
+                raise RuntimeError("Event Kind {} does not exist".format(kind))
+
+            if len(events) > i + 1 and events[i + 1].index == index:
+                # events are sorted by (index, height), so last event with the same index is the highest
+
+                # if the next event is at the same index, don't calculate result yet
+                continue
+            else:
+                possibleNewHeight = -heap[0] if heap else 0
+                if currentHeight != possibleNewHeight:
+                    currentHeight = possibleNewHeight
+                    result.append([index, possibleNewHeight])
+
+        return result
+
+
+def test():
+    sol = Solution()
+    buildings = [[2, 9, 10], [3, 7, 15], [5, 12, 12], [15, 20, 10], [19, 24, 8]]
+    actual = sol.getSkyline(buildings)
+    expected = [[2, 10], [3, 15], [7, 12], [12, 0], [15, 10], [20, 8], [24, 0]]
+    assert actual == expected
