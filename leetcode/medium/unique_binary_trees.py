bc3db9d0a23d044921c2cbe03105796f6ab0588a
Daniel Gerlach <daniel.gerlach1981@googlemail.com>
2021-10-17 10:43:44 +0200

    leetcode, medium, number of unique binary trees -> catalan numbers

--- leetcode/medium/unique_binary_trees.py
+++ leetcode/medium/unique_binary_trees.py
@@ -18,19 +18,63 @@ https://leetcode.com/problems/unique-binary-search-trees/
 """
 
 from collections import namedtuple
+from functools import lru_cache
 
 
 class Solution:
     def numTrees(self, n: int) -> int:
-        pass
+        def helper(k):
+            if k == 0 or k == 1:
+                return 1
+            result = 0
+            for i in range(k):
+                result += helper(i) * helper(k - 1 - i)
+            return result
+
+        return helper(n)
+
+    def numTreesDP(self, n: int) -> int:
+        if n < 2:
+            return 1
+        dp = [0] * (n + 1)
+        dp[0] = dp[1] = 1
+
+        for i in range(2, n + 1):
+            result = 0
+            for j in range(i):
+                result += dp[j] * dp[i - 1 - j]
+            dp[i] = result
+
+        return dp[-1]
+
+    @lru_cache(maxsize=None)
+    def numTreesMemoize(self, n: int) -> int:
+        if n == 0 or n == 1:
+            return 1
+        result = 0
+        for i in range(n):
+            result += self.numTreesMemoize(i) * self.numTreesMemoize(n - 1 - i)
+        return result
 
 
 Case = namedtuple("Case", ["n", "expected"])
 
 
 def test():
-    cases = [Case(3, 5)]
+    cases = [
+        Case(0, 1),
+        Case(1, 1),
+        Case(2, 2),
+        Case(3, 5),
+        Case(4, 14),
+        Case(5, 42),
+        Case(6, 132),
+        Case(40, 2622127042276492108820),
+    ]
     sol = Solution()
     for c in cases:
-        actual = sol.numTrees(c.n)
+        # actual = sol.numTreesMemoize(c.n)
+        # assert actual == c.expected
+
+        actual = sol.numTreesDP(c.n)
         assert actual == c.expected
